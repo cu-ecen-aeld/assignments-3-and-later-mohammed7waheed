8731ea39ab6c6e6b6a1a7ece7da89d43775d9f85
mohammed7waheed <mohammedwahdan66@gmail.com>
2023-09-24 06:49:01 -0700

    my commit

---
+++ finder-app/finder.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+echo "two arguments required"
+echo "directory path and string to be searched"
+exit 1
+else
+echo "2 arguments received successfully"
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ -d "$filesdir" ]
+then 
+echo "valid directory"
+else
+echo "directory not found enter a correct directory"
+exit 1
+fi
+
+cd $filesdir
+
+echo "The number of files are `ls | wc -l` and the number of matching lines are `grep -r "${searchstr}" | wc -l`"
---
+++ finder-app/writer.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]
+then
+echo "two arguments required"
+echo "files path and string to be inserted"
+exit 1
+else
+echo "2 arguments received successfully"
+fi
+
+writefile=$1
+writestr=$2
+directory=` dirname ${writefile} `
+
+if [ -d "${directory}" ]
+then
+echo "directory exists"
+else
+echo "creating directory first"
+mkdir $directory
+	if [ -d "${directory}" ]
+	then
+	echo "created directory"
+	else
+	echo "cannot create directory"
+	exit 1 
+	fi
+fi
+
+touch $writefile
+echo $writestr > $writefile
+
+if [ -f "$writefile" ]
+then
+echo "file created"
+else
+echo "cannot create file"
+exit 1
+fi
+
+
