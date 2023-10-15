062aea53e2062a41e41b0c2dd3a8c536ff0c82c6
mohammed7waheed <mohammedwahdan66@gmail.com>
2023-10-15 05:23:01 -0700

    assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,20 +14,62 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    	struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+	thread_func_args->thread_complete_success = false;
+    
+    	int wait_to_obtain_ms = thread_func_args->wait_to_obtain_ms;
+	int wait_to_release_ms = thread_func_args->wait_to_release_ms;
+	pthread_mutex_t *mutex = thread_func_args->mutex;
+	
+	
+	if (wait_to_obtain_ms < 0)
+		return thread_param;
+
+	if (wait_to_release_ms < 0)
+		return thread_param;
+
+	usleep(wait_to_obtain_ms * 1000);
+	pthread_mutex_lock(mutex);
+	
+	DEBUG_LOG("obtain lock");
+
+	usleep(wait_to_release_ms * 1000);
+	pthread_mutex_unlock(mutex);
+
+	thread_func_args->thread_complete_success = true;
+    
+
+    
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
+    ///**
+     //* TODO: allocate memory for thread_data, setup mutex and wait  
+     // arguments, pass thread_data to created thread
+     //* using threadfunc() as entry point.
+     //*
+     //* return true if successful.
+     //*
+     //* See implementation details in threading.h file comment block
+     //
+     
+     struct thread_data *pthread_data = malloc(sizeof(struct thread_data));
+  
+  	pthread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+	pthread_data->wait_to_release_ms = wait_to_release_ms;
+	pthread_data->mutex = mutex;   
+     
+     
+     	if (pthread_create(thread, NULL, threadfunc, pthread_data) == 0)
+	{
+		return true;
+	}
+     
+     
+     
     return false;
 }
 
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -62,8 +62,8 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir ${OUTDIR}/rootfs
-cd ${OUTDIR}/rootfs
+mkdir rootfs
+cd rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin 
 mkdir -p var/log
@@ -82,35 +82,33 @@ else
 fi
 
 # TODO: Make and install busybox
-make -j2 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make -j2 CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a ${OUTDIR}/rootfs/bin/busybox | grep "Shared library"
-
+SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
 # TODO: Add library dependencies to rootfs
-SYSROOT=`${CROSS_COMPILE}gcc --print-sysroot`
-cd ${OUTDIR}/rootfs
-cp  $SYSROOT/lib/ld-linux-aarch64.so.1 lib
-cp  $SYSROOT/lib64/ld-2.31.so lib64
-cp  $SYSROOT/lib64/libc.so.6 lib64
-cp  $SYSROOT/lib64/libc-2.31.so lib64
-cp  $SYSROOT/lib64/libm.so.6 lib64
-cp  $SYSROOT/lib64/libm-2.31.so lib64
-cp  $SYSROOT/lib64/libresolv.so.2 lib64
-cp  $SYSROOT/lib64/libresolv-2.31.so lib64
+
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.* ${OUTDIR}/rootfs/lib
+cp ${SYSROOT}/lib64/libc.so.* ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libm.so.* ${OUTDIR}/rootfs/lib64
+cp ${SYSROOT}/lib64/libresolv.so.* ${OUTDIR}/rootfs/lib64
+
+
 
 # TODO: Make device nodes
-cd ${OUTDIR}/rootfs
-sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 600 dev/console c 5 1
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/null" c 1 3
+sudo mknod -m 666 "${OUTDIR}/rootfs/dev/console" c 5 1
+
 
 # TODO: Clean and build the writer utility
 cd ${FINDER_APP_DIR}
 make clean
-make CROSS_COMPILE=${CROSS_COMPILE}
-cp writer ${OUTDIR}/rootfs/home
+make CROSS_COMPILE=${CROSS_COMPILE} all
+
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
@@ -120,13 +118,16 @@ cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home
 cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home
 cp ${FINDER_APP_DIR}/writer.sh ${OUTDIR}/rootfs/home
 
+
 # TODO: Chown the root directory
 cd ${OUTDIR}/rootfs
 sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+
 cd ${OUTDIR}/rootfs
-find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
-cd ..
-gzip initramfs.cpio
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
+
 
