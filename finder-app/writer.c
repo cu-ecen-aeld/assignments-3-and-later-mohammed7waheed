17a7fe0c1fb7185322e3eb073a36bdbe474bc9cb
mohammed7waheed <mohammedwahdan66@gmail.com>
2023-10-20 07:23:47 -0600

    h

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,53 +1,28 @@
-
-
-
-
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
 #include <syslog.h>
 
-int main(int argc, char **argv)
-{
 
+int main (int argc, char* argv[]){
 
-int num;
-char *writefile,*writestr;
-writefile=argv[1];
-writestr=argv[2];
-num = argc - 1;
-openlog(NULL, 0, LOG_USER);
-syslog(LOG_DEBUG,"Writing %s to %s", writestr, writefile);
-
-if (num != 2 ) {
-	printf("Invalid number of arguments %d\n", num );
-	syslog(LOG_ERR, "Invalid number of arguments %d", num);
-	exit(1);
+openlog(NULL,0,LOG_USER);
+FILE *file;
+if(argc<3){
+syslog(LOG_ERR,"number of arguments should be 2 not %d",argc);
+return 1;
+}else if (argc>3){
+syslog(LOG_ERR,"number of arguments should be 2 not %d",argc);
+return 1;
+}
+else {
+file = fopen(argv[1],"w+");
+if(file==NULL){
+syslog(LOG_ERR,"cannot open/create this file %s",argv[1]);
+return 1;
+}else {
+syslog(LOG_DEBUG,"Writing %s to %s",argv[2],argv[1]);
+fprintf(file,"%s",argv[2]);
 }
 
-int fd, fw;
-fd = open(writefile, O_RDWR);
-if (fd < 0) {
-	printf("Failed to open the file\n");
-	syslog(LOG_ERR, "Failed to open the file");
-	exit(1);
-} else {
-
-	fw = write(fd, writestr, strlen(writestr));
-	if (fw < 0) {
-		printf("Failed to write to the file\n");
-		syslog(LOG_ERR, "Failed to write to the file");
-		exit(1);
-	}
 }
-
-if (close (fd) == -1)
-	perror ("close");
-
+return 0;
 }
-
